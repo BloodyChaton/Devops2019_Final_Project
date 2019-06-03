d13cac630ae9a3f4d3504df5e543e13dee93068f
kraiem12 <krayem1989rim@gmail.com>
2019-06-03 15:17:43 +0200

    creation de la premiere version du google cloud production

---
+++ var.tf
@@ -0,0 +1,91 @@
+variable "project" {
+  default = "united-lane-241907"
+} 
+
+variable "region" {
+  default = "europe-north1"
+} 
+
+variable "zone" {
+  default = "europe-north1-a"
+} 
+
+variable "admin_name" {
+  default = "production-client"
+} 
+
+variable "admin_firewall" {
+  default = "production-admin-firewall"
+} 
+
+variable "ssh_keys" {
+  default = "production-ssh-key"
+}
+
+variable "admin_subnetwork_name" {
+  default = "production-admin-subnetwork"
+}
+
+variable "kube_subnetwork_name" {
+  default = "prod-kube-subnetwork-pod"
+}
+
+variable "kube_subnetwork_node_name" {
+  default = "prod-kube-subnetwork-node"
+}
+
+variable "kube_subnetwork_serv_name" {
+  default = "prod-kube-subnetwork-serv"
+}
+
+variable "kube_cluster_name" {
+  default = "prod-cluster"
+}
+
+
+variable "kube_node_pool_1" {
+  default = "prod-pool-1"
+}
+
+variable "kube_node_pool_2" {
+  default = "prod-pool-2"
+}
+
+variable "project_name" {
+  default = "projectfinal"
+}
+variable "master_ip" {
+  default = "10.5.0.0/24"
+} 
+
+variable "master_ipv4_cidr" {
+  default = "10.15.0.0/28" 
+} 
+
+variable "admin_network" {
+  default = "10.5.0.10"
+} 
+
+variable "admin_subnetwork" {
+  default = "10.5.0.0/21"
+} 
+
+variable "kube_subnetwork_pod" {
+  default = "10.6.0.0/21"
+} 
+
+variable "kube_subnetwork_node" {
+  default = "10.7.0.0/21"
+} 
+
+variable "kube_subnetwork_service" {
+  default = "10.8.0.0/21"
+} 
+
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC25G/wnpjYHounebrOuLmEjrb3FSpXCTUDATuBXfISLeg4LYUdl6Xdf5gEpnC4tu1vrWJ8Kx8OtJbcErKcaN6fa8x0M8O5C3xyuaAcnjc4wZsJExXZTLE7cuJrdVmtdrn6slA+bYzyecFb35h8S6gO1uyNGNgjbkwdPU/khKzqwHd2gbxg56NNQFMFGwlLV2Lp9BubGD+ksMwUS9G81c0F6qEgdJ3bPfOql03qEwA+HeMdBWlXaA2lPpiV9i6MgbVNGLA6qeUL1sMp3jA5FdRq9SOxVO9fncz9Pbm04k8li0AtN7w/4lYtC0SzL8Y+5zirc32+ovCe9eFWde7Vz0M5 adminl@localhost.localdomain"
+}
+
+variable "ssh_user" {
+  default = "adminl"
+}
